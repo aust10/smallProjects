4f35632991507553af53299d248cd71358195c70
austen cote <coteausten@gmail.com>
2020-03-04 10:23:40 -0800

    updating lab_23.py

--- python_labs/lab_23.py
+++ python_labs/lab_23.py
@@ -1,16 +1,16 @@
-with open('test_contacts.csv', 'r') as file:
+with open('test_contacts_out.csv', 'r') as file:
     lines = file.read().split('\n')
-    print (lines)
+    # print (lines)
     lines = [item.lower() for item in lines]
-    print(lines)
+    # print(lines)
     #print(lines)
 
 things = [] 
-
+contacts = []
 for i in range(len(lines)):
     key_things = lines[0]
     key_things = key_things.split(',')
-    print(key_things)
+    # print(key_things)
     row = lines[i]
     row = row.split(',')
     row = dict(zip(key_things,row))
@@ -22,15 +22,15 @@ def export_list():
     
     things[0].values()
     list(things[0].values())
-    list(things[1].values())
+    list(things[0].values())
     things[0].keys()
     ','.join(things[0].keys())
     ','.join(things[0].values())
-    mylist.append(','.join(things[0].keys()))
+    # mylist.append(','.join(things[0].keys()))
     for i in range(len(things)):# this for loop makes it loop through everything and not just line one and two
         mylist.append(','.join(things[i].values()))
-    '\n'.join(mylist)
-    print(mylist)
+        '\n'.join(mylist)
+    # print(mylist)
     # print (mylist)
     with open('test_contacts_out.csv', 'w') as contact_list:
        contact_list.write('\n'.join(mylist))
@@ -40,15 +40,15 @@ def input_stuff():
     #user = []
     user = input('what is your name? '), input('what is your favorite fruit? '), input('what is your favorite color?')
     # new_row = ''.join(user_input).split(',')
-    # user.append(user_input)
+    # # user.append(user_input)
     # user = [list(x) for x in user]
     # user = [value for sec_list in user for value in sec_list]
-    print(user)
+    # print(user)
     new_row = dict(zip(key_things, user))
     things.append(new_row)
-    print(things)
+    print('this is things',things)
     export_list()
-    #return new_row
+    return new_row
 
 def find():
     find_it = input('What is the name of the person you would like to find? ')
@@ -59,6 +59,7 @@ def find():
             return n
         
 def update():
+    print(things)
     update = input('what contact do you want to update? ')
     atrabute = input('what option do you want to update? ')
     new_value = input('what would you like to replace it with? ')
@@ -66,16 +67,30 @@ def update():
         if n['name'] == update:
             if atrabute in n:
                 n[atrabute] = new_value
-                print (n)
+                
                 export_list()
             # return n  
             
+# def update_contact():
+#     print("Update a contact, Name-Game-Pet")
+#     up = input("Who do you want to update? ")
+#     change = input("What do you want to update?: ")
+#     new_name = input("What is the new Name? ")
+#     for duck in con2:
+#         if duck['name'] == up:
+#             if change in duck:
+#                 duck[change] == new_name
+#                 print(duck[change]) #this should be print duck not duck change
+#                 print(new_name)
+
+
+
 def delete():
     delete = input('What contatct name do you want to delete? ')
     for i in range(len(things)):
         if things[i]['name'] == delete:
             del things[i]
-            print(things)
+            # print(things)
             export_list()
             return things
 
